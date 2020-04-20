dbae53da96de07f84fcb9920fd83c625c0e8fc54
anapaulacsv <58363368+anapaulacsv@users.noreply.github.com>
2020-04-20 18:38:17 -0300

    Api

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+# 
 
-# Create your views here.
+class BookList(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer 
