e19552d4c253db6cebda723bf961770f850f7a44
anapaulacsv <58363368+anapaulacsv@users.noreply.github.com>
2020-04-20 15:39:57 -0300

    Book Select, Update and Delete

--- app/tests/test_db.py
+++ app/tests/test_db.py
@@ -42,11 +42,30 @@ class BookTest(TestCase):
         bk1 = Book(name='True story', edition=111, pub_year=1981)
         bk1.save()
         bk1.authors.set([aut1, aut2])
+        bk1.save()
         bk2 = Book(name='Happy day', edition=27, pub_year=2002)
         bk2.save()
         bk2.authors.set([aut1])
+        bk2.save()
 
     def test_select(self):
         #Select
-            
+        bk1= Book.objects.get(name="True story")
+        self.assertEqual(bk1.name, "True story")   
+        
+    def test_update(self):
+        #Update
+        bk1 = Book.objects.get(name="True story")
+        bk1.name = "True story 2"
+        bk1.save()
+        bk1 = Book.objects.get(name="True story 2")
+        self.assertEqual(bk1.name, "True story 2")
+
+    def test_delete(self):
+        #Delete
+        bk2 = Book.objects.get(name="Happy day")
+        bk2.delete()
+        #Test if the delete is true
+        with self.assertRaisesMessage(ObjectDoesNotExist,'Book matching query does not exist.'):
+            Book.objects.get(name="Happy day")
 
